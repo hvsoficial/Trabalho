e0f0990ec672f52c02b8621c727a76f435fb6d60
Henrique Souza <henriquevaleirodsouza@hotmail.com>
2020-11-22 20:21:32 -0300

    up

---
+++ oleo_web - usuario - Copia - Copia/src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FiArrowRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+import logo from '../images/logo.svg'
+
+import '../styles/pages/landing.css'
+
+const Landing: React.FC = () => {
+	return (
+		<div id="page-landing">
+			<div className="content-wrapper">
+				{/*<img src={logo} alt="Logo Happy" />*/}
+
+				<main>
+					<h1>Ajude a reciclar</h1>
+					<p>Contribua para um planeta melhor</p>
+				</main>
+
+				<div className="location">
+					<strong> </strong>
+					<span> </span>
+				</div>
+
+				<Link to="/app" className="enter-app"><FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" /></Link>
+			</div>
+		</div>
+	)
+}
+
+export default Landing
